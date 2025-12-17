78f2a3989cd58fe08e72b59a40a622300c6043ce
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-12-17 07:39:40 +0100

    Essential

---
+++ Essential.cpp
@@ -0,0 +1,19 @@
+//
+//  Essential.cppggg
+//
+//
+//  Created by M'Barek Benraiss on 16.12.25.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
+
+
+
+
+
