fa2e1bfb0dc84536f15ee2748b8dbcfbf3bb2164
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-12-17 06:54:39 +0100

    Initial Commit

---
+++ Essential/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Essential
+//
+//  Created by M'Barek Benraiss on 17.12.25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
