c3997d89611bbcfefadb2c687e754f982d6e0ec8
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-12-26 20:35:17 +0100

    strings

--- Essential.cpp
+++ Essential.cpp
@@ -4,21 +4,24 @@
 //
 //  Created by M'Barek Benraiss on 16.12.25.
 //
-
-#include "ArrayDSA/ArrayMenuusing_cpp/ArrayMenu.hpp"
-#include "ArrayDSA/Array_C++/Array.hpp"
-
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include "ArrayDSA/ArrayMenuusing_cpp/ArrayMenu.hpp"
+#include "ArrayDSA/Array_C++/Array.hpp"
+#include "ArrayDSA/array_ADT/ArrayADT.h"
+#include "ArrayDSA/arrays/ArraysRep.h"
+#include "ArrayDSA/recursion/Combinatorics.h"
+#include "ArrayDSA/recursion/Recursion.h"
 
 int main(int argc, const char *argv[])
 {
   std::cout << "Start Program!" << '\n';
 
   // Template Array demo
-  arrayTemplateDemo();
+  // arrayTemplateDemo();
 
-  // arraysMenu demo C++ version 
+  // arraysMenu demo C++ version
   // arraysMenu();
 
   std::cout << "End Program!" << '\n';
